8f2f88a5e122bbacfb8e87177201250e096426e7
*xq65yDM0EB <izawa@atali.jp>
2021-08-31 23:12:49 +0900

    server編集

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express') //expressモジュールの読み込み
 const app = express() //アプリケーションオブジェクトをapp変数に格納する
-const port = 3000 //port番号の設定
+const http = require('http').Server(app); //httpモジュールの読み込み
+const io = require('socket.io')(http);
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+app.use(express.static('public'));//ルートの静的ファイルへのアクセス許可
 
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-  })
+http.listen(PORT, function() { //3000番ポートでアクセスを待つ
+    console.log('server listening. PORT:' + PORT);
+  });
+
+// GET リクエストを処理する
+// ===========================================================
+app.get(`/`, (req, res) => { 
+    res.sendFile(__dirname + '/views/index.html'); //"/"にアクセスがあったらindex.htmlを返却
+  });
