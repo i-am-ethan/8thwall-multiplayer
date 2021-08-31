bbb503135a0e3bbbed4918e5b20da4afefd5ee76
*xq65yDM0EB <izawa@atali.jp>
2021-08-31 22:39:12 +0900

    コメント追加

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express') //expressモジュールの読み込み
+const app = express() //アプリケーションオブジェクトをapp変数に格納する
+const port = 3000 //port番号の設定
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
